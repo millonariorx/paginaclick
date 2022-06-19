ceedd3148cfdac52f1d8cbece9682ccc7c8cee7c
millonariorx <betterbeat00@gmail.com>
2022-06-18 19:06:41 -0500

    paginaclick

---
+++ assets/js/funciones.js
@@ -0,0 +1,6 @@
+var num=/*Editar -->*/ 573046494527;                                                                    
+var texto=/*Editar -->*/"Hola Quiero cotizar para el diseño de una pagina web";                                                               
+
+function whats(){
+    window.open('https://api.whatsapp.com/send?phone='+num+'&text='+texto+'', '_blank');
+};
